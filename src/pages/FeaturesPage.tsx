fed5ce7beea1aec48353525bfb6f0b8419ab5276
ROHTASH SINGH <rohtash.singh.osv@fedex.com>
2025-09-30 16:04:59 +0530

    Create Task adding using json-server | fake api

--- src/pages/FeaturesPage.tsx
+++ src/pages/FeaturesPage.tsx
@@ -1,14 +1,13 @@
 import '../assets/styles/feature.css';
-import React from 'react';
-import { Link, Outlet } from 'react-router-dom';
 import Menu from '../layouts/Menu';
-import CommonProgramFormatComponent from '../components/common/components/CommonProgramFormatComponent';
+import Home from './TaskItems.tsx/Home';
 
 const FeaturesPage = () => (
   <>
     <Menu />
-    <h2 style={{ padding: '10px' }}>Javascript Interview question and answer</h2>
-    <CommonProgramFormatComponent />
+    <Home />
+    {/* <h2 style={{ padding: '10px' }}>Javascript Interview question and answer</h2>
+    <CommonProgramFormatComponent /> */}
   </>
   
 );
